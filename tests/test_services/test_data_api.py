1266d1fac009414abab474644212fbc1f182211c
nizbel <kingbowserii@gmail.com>
2020-03-21 21:23:25 -0300

    Endpoint 2, implementado bem básico (#7)
    * Preparando model CityReport
    
    * Adicionando teste para endpoint
    
    * Implementando endpoint #2, situação por estado
    
    * Models atualizados
    
    * Adicionando save a cada model
    
    * Corrigindo models
    
    * Corrigindo nome da tabela
    
    * Atualizando dados do teste
    
    * Alterando CityReport para City
    
    * Corrigindo números
    
    * Preparando campos para o teste

---
+++ tests/test_services/test_data_api.py
@@ -0,0 +1,64 @@
+import datetime
+import json
+from unittest import TestCase
+from app import app, db
+from mock import patch
+from models import City
+from tests.runner import clear_db
+
+
+class TestDataApi(TestCase):
+
+    def setUp(self):
+        self.app_context = app.test_request_context()
+        self.app_context.push()
+        self.client = app.test_client()
+        self.app = app
+        db.create_all()
+        self.db = db
+
+    def tearDown(self):
+        clear_db(self.db)
+
+    def test_return_cases_by_state_without_reports(self):
+        resp = self.client.get('/data_api/v1/data/state/SP')
+        data = json.loads(resp.get_data(as_text=True))
+
+        assert len(data) == 4
+
+        assert 'activeCases' in data
+        assert 'suspectedCases' in data
+        assert 'recoveredCases' in data
+        assert 'deaths' in data
+
+        assert data['activeCases'] == 0
+        assert data['suspectedCases'] == 0
+        assert data['recoveredCases'] == 0
+        assert data['deaths'] == 0
+
+    def test_return_cases_by_state_with_reports(self):
+        # Generate test data
+        City().save(self.db.session, city='Igarapava', state='SP',
+                    country='Brasil', total_cases=45, suspects=35, refuses=3, deaths=2, recovered=1)
+        City().save(self.db.session, city='Franca', state='SP',
+                    country='Brasil', total_cases=50, suspects=35, refuses=3, deaths=1, recovered=1)
+
+        # Should not include this data
+        City().save(self.db.session, city='Uberaba', state='MG',
+                    country='Brasil', total_cases=50, suspects=35, refuses=3, deaths=1, recovered=1)
+        self.db.session.commit()
+
+        resp = self.client.get('/data_api/v1/data/state/SP')
+        data = json.loads(resp.get_data(as_text=True))
+
+        assert len(data) == 4
+
+        assert 'activeCases' in data
+        assert 'suspectedCases' in data
+        assert 'recoveredCases' in data
+        assert 'deaths' in data
+
+        assert data['activeCases'] == 14
+        assert data['suspectedCases'] == 70
+        assert data['recoveredCases'] == 2
+        assert data['deaths'] == 3
