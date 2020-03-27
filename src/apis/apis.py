b81c271c2fac833130459363e44fa4126bb566de
Everton Castro <evertoncastro.sp@gmail.com>
2020-03-27 15:13:49 -0300

    Force https in swagger statics for staging and production (#98)

--- src/apis/apis.py
+++ src/apis/apis.py
@@ -1,21 +1,41 @@
-from flask import Blueprint
+import os
+from flask import Blueprint, url_for
 from flask_restplus import Api as ApiRestPlus
 from apis.auth import endpoints as auth_endpoints
 from apis.data import endpoints as data_endpoints
 from apis.test_point import endpoints as test_point_endpoints
 
 
-client_api = ApiRestPlus(
+class SecureApiRestPlus(ApiRestPlus):
+    """
+    Custom Flask Rest Plus API Class
+    """
+
+    @property
+    def specs_url(self):
+        """
+        Method overwritten to force HTTPS on swagger static resources
+        for specific environments
+        :return:
+        """
+        scheme = 'https' if os.environ.get('FLASK_ENV') in \
+            ['staging', 'production'] else 'http'
+        return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
+
+
+client_api = SecureApiRestPlus(
     Blueprint('Data API', __name__),
     title='Data API',
     version='1.0',
     description='Endpoints to serve data to front end clients'
 )
 
-# Bind each namespace for data here
+
+# Bind each namespace here
 auth_endpoints.bind(client_api)
 data_endpoints.bind(client_api)
 test_point_endpoints.bind(client_api)
+# Bind each namespace here
 
 
 def load_apis(app):
